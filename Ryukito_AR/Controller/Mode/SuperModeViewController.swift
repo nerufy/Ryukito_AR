a629d3e0caa01bf9cc1423cf0264c4640b8fe023
nerufy <nerufy1031@gmail.com>
2020-10-06 12:05:47 +0900

    MVCモデルへの置換、及び各コントローラーのSuperクラスの作成

---
+++ Ryukito_AR/Controller/Mode/SuperModeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SuperViewController.swift
+//  Ryukito_AR
+//
+//  Created by kouki on 2020/10/05.
+//  Copyright © 2020 又吉琉稀斗. All rights reserved.
+//
+class SuperModeViewController: SuperViewController {
+}
